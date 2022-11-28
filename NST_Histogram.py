2d8a3d30f15ba485dfa3282aa6eff25e127e1cf7
your name <pthieu2111@gmail.com>
2022-11-28 15:40:50 +0700

    out of memory

--- NST_Histogram.py
+++ NST_Histogram.py
@@ -38,104 +38,86 @@ def getContentSize(path_image):
 
 
 path_style = 'style-images/candy.jpg'
-root_img = 'content-jpg'
-# getContentSize('content-images/0003.png')
-# sys.exit()
-
-for name_imge in os.listdir(root_img):
-    path_img = os.path.join(root_img, name_imge)
-    h, w = getContentSize(path_img)
-
-    device = torch.device("cuda" if torch.cuda.is_available else 'cpu')
-    # image_size = 256
-
-    loader = transforms.Compose(
-        [
-            transforms.Resize((h, w)),
-            transforms.ToTensor(),
-        ]
-    )
+path_img = 'content-jpg/0003.jpg'
 
 
-    img = Image.open(path_style).convert('RGB')
-    content = Image.open(path_img).convert('RGB')
-    original_img = load_image(path_img)
-    style_img_tranform = cal_mean_std.style_transformer2(content, img)
+h, w = getContentSize(path_img)
+
+device = torch.device("cuda" if torch.cuda.is_available else 'cpu')
+image_size = 256
+
+loader = transforms.Compose(
+    [
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+    ]
+)
 
 
-    # convert from nparray to PIL
-    style_img_PIL = Image.fromarray(style_img_tranform)
-    style_img = loader(style_img_PIL).unsqueeze(0).to(device)
-
-    # Create generated image by clone content image or random
-    # generated = torch.randn(original_img.shape, device=device, requires_grad=True)
-    generated = original_img.clone().requires_grad_(True)
+img = Image.open(path_style).convert('RGB')
+content = Image.open(path_img).convert('RGB')
+original_img = load_image(path_img)
+style_img_tranform = cal_mean_std.style_transformer2(content, img)
 
 
-    model = VGG().to(device).eval()
+# convert from nparray to PIL
+style_img_PIL = Image.fromarray(style_img_tranform)
+style_img = loader(style_img_PIL).unsqueeze(0).to(device)
 
-    # Hyperparameters
-    total_steps = 6000
-    learning_rate = 0.001
-    alpha = 1
-    beta = 0.01
-    optimizer = optim.Adam([generated], lr=learning_rate)
+# Create generated image by clone content image or random
+# generated = torch.randn(original_img.shape, device=device, requires_grad=True)
+generated = original_img.clone().requires_grad_(True)
 
-    # Create dir to save image
-    # Get style name
-    style_name = getFileName(path_style)
-    image_name = getFileName(path_img)
-    path_dir_save_image = f'./output-images/{style_name}/{image_name}'
-    if os.path.exists(path_dir_save_image) == False:
-        os.makedirs(path_dir_save_image)
 
-    # get file name
-    file_name = getFileName(path_img)
+model = VGG().to(device).eval()
 
-    def cal_loss(gen_feature, orig_feature, style_feature):
+# Hyperparameters
+total_steps = 6000
+learning_rate = 0.001
+alpha = 1
+beta = 0.01
+optimizer = optim.Adam([generated], lr=learning_rate)
+
+# Get style name
+style_name = getFileName(path_style)
+image_name = getFileName(path_img)
+
+# Create dir to save image
+path_dir_save_image = f'./output-images/{style_name}/{image_name}'
+if os.path.exists(path_dir_save_image) == False:
+    os.makedirs(path_dir_save_image)
+
+# get file name
+file_name = getFileName(path_img)
+
+for step in range(total_steps):
+    generated_features = model(generated)
+    original_img_features = model(original_img)
+    style_features = model(style_img)
+
+    style_loss = original_loss = 0
+
+    for gen_feature, orig_feature, style_feature in zip(generated_features,
+                                            original_img_features, style_features):
         batch_size, channel, height, width = gen_feature.shape
-        original_loss = torch.mean((gen_feature - orig_feature) ** 2)
+        original_loss += torch.mean((gen_feature - orig_feature)**2)
 
         # Computer Gram Matrix
-        G = gen_feature.view(channel, height * width).mm(
-            gen_feature.view(channel, height * width).t()
+        G = gen_feature.view(channel, height*width).mm(
+            gen_feature.view(channel, height*width).t()
         )
 
-        A = style_feature.view(channel, height * width).mm(
-            style_feature.view(channel, height * width).t()
+        A = style_feature.view(channel, height*width).mm(
+            style_feature.view(channel, height*width).t()
         )
-        style_loss = torch.mean((G - A) ** 2)
-        return original_loss, style_loss
-
-    for step in range(total_steps):
-        generated_features = model(generated)
-        original_img_features = model(original_img)
-        style_features = model(style_img)
-
-        style_loss = original_loss = 0
-        # original_loss, style_loss = cal_loss(generated_features, original_img_features, style_features)
-
-        for gen_feature, orig_feature, style_feature in zip(generated_features,
-                                                original_img_features, style_features):
-            batch_size, channel, height, width = gen_feature.shape
-            original_loss += torch.mean((gen_feature - orig_feature)**2)
-
-            # Computer Gram Matrix
-            G = gen_feature.view(channel, height*width).mm(
-                gen_feature.view(channel, height*width).t()
-            )
-
-            A = style_feature.view(channel, height*width).mm(
-                style_feature.view(channel, height*width).t()
-            )
-            style_loss += torch.mean((G -A)**2)
+        style_loss += torch.mean((G -A)**2)
 
 
-        total_loss = alpha*original_loss + beta*style_loss
-        optimizer.zero_grad()
-        total_loss.backward()
-        optimizer.step()
+    total_loss = alpha*original_loss + beta*style_loss
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
 
-        if step % 200 ==0:
-            print(total_loss)
-            save_image(generated, os.path.join(path_dir_save_image, f'{file_name}_step_{step}.png'))
+    if step % 200 ==0:
+        print(total_loss)
+        save_image(generated, os.path.join(path_dir_save_image, f'{file_name}_step_{step}.png'))
